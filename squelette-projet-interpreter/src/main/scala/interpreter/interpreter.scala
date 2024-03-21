ad03727de38d8b5f7cbc85aee8383d9b0ed273bc
oscar isorez <oisorez@b40-011m05.istic.univ-rennes1.fr>
2024-03-21 09:36:16 +0100

    update

--- squelette-projet-interpreter/src/main/scala/interpreter/interpreter.scala
+++ squelette-projet-interpreter/src/main/scala/interpreter/interpreter.scala
@@ -3,7 +3,7 @@ package interpreter
 /*
  * VEUILLEZ INSCRIRE CI-DESSOUS VOTRE NOM ET VOTRE PRENOM :
  * 
- * ETUDIANT 1 :
+ * ETUDIANT 1 : ISOREZ Oscar
  * 
  * ETUDIANT 2 :
  * 
@@ -42,7 +42,13 @@ object Interpreter {
    * la valeur par défaut si la variable v n'est pas présente dans la mémoire mem
    */
   // TODO TP2
-  def lookUp(v: Variable, mem: Memory): Value = ???
+  def lookUp(v: Variable, mem: Memory): Value = 
+    mem match {
+      case (variable, value) :: tail if variable == v => value
+      case _ :: tail => lookUp(v, tail)
+      case Nil => NlValue
+
+    }
 
   /**
    * @param v : une variable
@@ -51,7 +57,8 @@ object Interpreter {
    * @return la mémoire modifiée par l'affectation [v->d]
    */
   // TODO TP2
-  def assign(v: Variable, d: Value, mem: Memory): Memory = ???
+  def assign(v: Variable, d: Value, mem: Memory): Memory = 
+    (v, d) :: mem.filterNot(_._1 == v)
 
   /**
    *  TRAITEMENT DES EXPRESSIONS DU LANGAGE WHILE
@@ -62,7 +69,27 @@ object Interpreter {
    * @return la valeur de l'expression
    */
   // TODO TP2
-  def interpreterExpr(expression: Expression, mem: Memory): Value = ???
+  def interpreterExpr(expression: Expression, mem: Memory): Value = 
+    expression match {
+      case Nl => NlValue
+      case Cst(name) => CstValue(name)
+      case VarExp(name) => lookUp(Var(name), mem)
+      case Cons(arg1, arg2) => ConsValue(interpreterExpr(arg1, mem), interpreterExpr(arg2, mem))
+      case Hd(arg) => interpreterExpr(arg, mem) match {
+        case ConsValue(arg1, arg2) => arg1
+        case _ => throw new Exception("Hd expects a ConsValue")
+      }
+      case Tl(arg) => interpreterExpr(arg, mem) match {
+        case ConsValue(arg1, arg2) => arg2
+        case _ => throw new Exception("Tl expects a ConsValue")
+      }
+      case Eq(arg1, arg2) => (interpreterExpr(arg1, mem), interpreterExpr(arg2, mem)) match {
+        case (CstValue(name1), CstValue(name2)) => if (name1 == name2) CstValue("true") else CstValue("false")
+        case (ConsValue(arg11, arg12), ConsValue(arg21, arg22)) => if (arg11 == arg21 && arg12 == arg22) CstValue("true") else CstValue("false")
+        case _ => throw new Exception("Eq expects two CstValue or two ConsValue")
+      }
+    }
+
 
   /**
    * la fonction interpreterExpr ci-dessus calcule la valeur associée à une expression
@@ -73,7 +100,12 @@ object Interpreter {
    * @return l'AST décrivant l'expression de cette valeur
    */
   // TODO TP2
-  def valueToExpression(value: Value): Expression = ???
+  def valueToExpression(value: Value): Expression = 
+    value match {
+      case NlValue => Nl
+      case CstValue(name) => Cst(name)
+      case ConsValue(arg1, arg2) => Cons(valueToExpression(arg1), valueToExpression(arg2))
+    }
   
 
   /**
@@ -87,7 +119,14 @@ object Interpreter {
    * @return la mémoire après l'interprétation de command
    */
   // TODO TP2
-  def interpreterCommand(command: Command, memory: Memory): Memory = ???
+  def interpreterCommand(command: Command, memory: Memory): Memory = 
+    command match {
+      case Nop => memory
+      case Set(variable, expression) => assign(variable, interpreterExpr(expression, memory), memory)
+      case While(condition, body) => interpreterCommand(While(condition, body), interpreterCommands(body, memory))
+      case For(count, body) => interpreterCommand(For(count, body), interpreterCommands(body, memory))
+      case If(condition, then_commands, else_commands) => interpreterCommand(If(condition, then_commands, else_commands), interpreterCommands(then_commands, memory))
+    }
   
   
   /**
@@ -96,7 +135,11 @@ object Interpreter {
    * @return la mémoire après l'interprétation de la liste de commandes
    */
   // TODO TP2
-  def interpreterCommands(commands: List[Command], memory: Memory): Memory = ???
+  def interpreterCommands(commands: List[Command], memory: Memory): Memory = 
+    commands match {
+      case command :: tail => interpreterCommands(tail, interpreterCommand(command, memory))
+      case Nil => memory
+    }
   
 
   /**
@@ -110,7 +153,8 @@ object Interpreter {
    * @return une mémoire associant chaque valeur à la variable d'entrée correspondant
    */
   // TODO TP2
-  def interpreterMemorySet(vars: List[Variable], vals: List[Value]): Memory = ???
+  def interpreterMemorySet(vars: List[Variable], vals: List[Value]): Memory = 
+    vars.zip(vals).map{case (v, d) => (v, d)}
   
 
   /**
@@ -119,7 +163,8 @@ object Interpreter {
    * @return la liste des valeurs des variables de sortie
    */
   // TODO TP2
-  def interpreterMemoryGet(vars: List[Variable], memory: Memory): List[Value] = ???
+  def interpreterMemoryGet(vars: List[Variable], memory: Memory): List[Value] = 
+    vars.map(v => lookUp(v, memory))
 
   /**
    * @param program : un AST décrivant un programme du langage WHILE
@@ -127,8 +172,22 @@ object Interpreter {
    * @return la liste des valeurs des variables de sortie
    */
   // TODO TP2
-  def interpreter(program: Program, vals: List[Value]): List[Value] = ???
-  
+  def interpreter(program: Program, vals: List[Value]): List[Value] =
+    program match {
+      case Progr(in, body, out) => interpreterMemoryGet(out, interpreterCommands(body, interpreterMemorySet(in, vals)))
+    }
+
+
+
+
+
+
+
+
+
+    
+
+    
   
   
 
